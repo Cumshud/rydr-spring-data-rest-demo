f5b98da6fb52f2f94f2bb17a225f963ae6126bf6
Cumshud <cumsud.esedzade@gmail.com>
2022-09-19 07:36:07 +0400

    Initial commit

---
+++ src/test/java/az/spring/rest/rydrspringdatarestdemo/RydrSpringDataRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.spring.rest.rydrspringdatarestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RydrSpringDataRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
